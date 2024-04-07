9f5dcc991a61d9f75b966b7b8eb0f3e36ca9626d
xssnd <radietsa@gmail.com>
2024-04-08 00:04:20 +0700

    add new items

---
+++ api/db_config.php
@@ -0,0 +1,6 @@
+<?php
+define(DB_USER, "root");
+define(DB_PASSWORD, "admin");
+define(DB_DATABASE, "phpcrud");
+define(DB_HOST, "localhost");
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
